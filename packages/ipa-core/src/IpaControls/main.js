bb184a8cbd7bfe4dfdcd0b2eda77d736f2f9ce3b
scottmollon <scott.mollon@invicara.com>
2021-01-06 14:44:15 -0500

    exposing more controls

--- packages/ipa-core/src/IpaControls/main.js
+++ packages/ipa-core/src/IpaControls/main.js
@@ -3,14 +3,35 @@ import GenericMatButton from './GenericMatButton'
 import EnhancedFetchControl from './EnhancedFetchControl'
 import {StackableDrawer} from './StackableDrawer'
 import FancyTreeControl from './FancyTreeControl'
+import ChartStack from './ChartStack'
+import CrossEntitySearch from './CrossEntitySearch'
+import {CreatableScriptedSelects} from './EnhancedCreatableScriptedSelects'
+import {ScriptedLinkedSelects} from './EnhancedScriptedLinkedSelects'
+import {ScriptedSelects} from './EnhancedScriptedSelects'
+import GenericIframe from './GenericIframe'
+import Image from './Image'
+import MiniButton, {MiniIconButton} from './MiniButton'
+import ScriptedChart from './ScriptedChart'
+import SimpleTextThrobber from './SimpleTextThrobber'
 
 const IpaControls = {
     SimpleTable,
     IpaButton: GenericMatButton,
+    IpaMiniButton: MiniButton,
+    IpaMiniIconButton: MiniIconButton,
     GenericMatButton,
     EnhancedFetchControl,
     StackableDrawer,
-    FancyTreeControl
+    FancyTreeControl,
+    ChartStack,
+    CrossEntitySearch,
+    CreatableScriptedSelects,
+    ScriptedLinkedSelects,
+    ScriptedSelects,
+    Iframe: GenericIframe,
+    Image,
+    ScriptedChart,
+    SimpleTextThrobber
 }
 
 export default IpaControls
