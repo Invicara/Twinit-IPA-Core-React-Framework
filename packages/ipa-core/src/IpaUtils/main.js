5e89c30a4c1b98de9baad2ff1dd7dc56c44972b4
scottmollon <scott.mollon@invicara.com>
2021-01-08 11:35:47 -0500

    exposing ScriptCache in IpaUtils

--- packages/ipa-core/src/IpaUtils/main.js
+++ packages/ipa-core/src/IpaUtils/main.js
@@ -4,10 +4,12 @@ import SchemaHelper from './SchemaHelper'
 import FileHelpers from './FileHelpers'
 import ScriptHelper from './ScriptHelper'
 import TreeRendererHelper from './TreeRendererHelper'
+import ScriptCache from './script-cache'
 
 const IpaUtils = {
     compare,
     ScriptHelper,
+    ScriptCache,
     FileHelpers,
     SchemaHelper,
     TreeRendererHelper
