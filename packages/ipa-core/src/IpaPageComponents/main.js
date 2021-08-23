211a55aa785738ef20eb69bfa0d2c86f33373b6d
Rodolfo Leandro Caputo <rodcap.93@gmail.com>
2021-08-22 20:48:18 -0700

    Adding configurable icons to action panel:

--- packages/ipa-core/src/IpaPageComponents/main.js
+++ packages/ipa-core/src/IpaPageComponents/main.js
@@ -2,12 +2,14 @@ import withEntitySearch from './entities/WithEntitySearch'
 import EntitySelectionPanel from './entities/EntitySelectionPanel'
 import {TreeSelectMode} from './entities/EntitySelectionPanel'
 import EntityDataStack from './entities/EntityDataStack'
+import EntityActionsPanel from "./entities/EntityActionsPanel";
 
 const IpaPageComponents = {
     withEntitySearch,
     EntitySelectionPanel,
     EntityDataStack,
-    TreeSelectMode
+    TreeSelectMode,
+    EntityActionsPanel
 }
 
 export default IpaPageComponents
