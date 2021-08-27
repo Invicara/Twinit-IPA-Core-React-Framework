e799d83aa533a51961fbfb7d492ac881a06ee349
scottmollon-inv <scott.mollon@invicara.com>
2021-08-27 09:21:22 -0400

    sisense debug

--- packages/ipa-core/src/IpaPageComponents/InternalPages.js
+++ packages/ipa-core/src/IpaPageComponents/InternalPages.js
@@ -5,8 +5,6 @@ import UploadFilesWizard from './files/UploadFilesWizard'
 import ScriptRunnerView from './solutionMgmt/ScriptRunnerView'
 import DownloadsView from './DownloadsView'
 import UserGroupView from './users/UserGroupView'
-import SisenseLoginPage from './sisense/SisenseLoginPage'
-import SisenseLogoutPage from './sisense/SisenseLogoutPage'
 import DatasourcesView from './datasources/DatasourcesView'
 
 const InternalPages = {
