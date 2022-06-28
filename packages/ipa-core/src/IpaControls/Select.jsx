7259c01d71806c408ba3b6cae71e9fd41fd9148c
Cesar Fraisseix <cesar.fraisseix@invicara.com>
2022-06-28 10:17:58 +0100

    DBM-1794
    CreatableSelect import was an object instead of a component, CreatableSelect.default yields the component

--- packages/ipa-core/src/IpaControls/Select.jsx
+++ packages/ipa-core/src/IpaControls/Select.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {default as ReactSelect} from 'react-select'
-import CreatableSelect from 'react-select/creatable'
+import CreatableSelect from 'react-select/creatable';
 import { selectStyles } from './private/selectStyles'
 import clsx from "clsx";
 import _, { functionsIn } from 'lodash';
@@ -12,7 +12,26 @@ import {any, array, bool, func, object, shape, string} from 'prop-types'
 
 const Select = props => {
 
-  let Component = props.creatable ? CreatableSelect : ReactSelect
+  let selectProps = {
+    styles: {
+      control: selectStyles,
+      option: optionStyle,
+      ...props.styles
+    },
+    id: props.id,
+    isMulti: props.isMulti,
+    value: props.value,
+    onChange: props.onChange,
+    options: props.options,
+    classNamePrefix: 'ipa-select',
+    className: 'select-element',
+    closeMenuOnSelect: props.closeMenuOnSelect,
+    isClearable: props.isClearable,
+    placeholder: props.placeholder || props.label && `Select a ${props.label}`,
+    isDisabled: props.isDisabled,
+    menuPlacement: props.menuPlacement,
+    menuPosition: props.menuPosition
+  }
 
   let optionStyle = (styles, { data }) => {
     return {
@@ -21,31 +40,20 @@ const Select = props => {
     }
   };
 
+  console.log("Select props", props);
+  console.log("CreatableSelect", CreatableSelect);
+  console.log("ReactSelect", ReactSelect);
+
 
   return (
     <div className='ipa-select'>
       {props.labelProps && (
         <ControlLabel {...props.labelProps} required={props.required}/>
       )}
-      <Component
-        styles={{
-          control: selectStyles,
-          option: optionStyle,
-          ...props.styles
-        }}
-        isMulti={props.isMulti}
-        value={props.value}
-        onChange={props.onChange}
-        options={props.options}
-        classNamePrefix='ipa-select'
-        className='select-element'
-        closeMenuOnSelect={props.closeMenuOnSelect}
-        isClearable={props.isClearable}
-        placeholder={props.placeholder || props.label && `Select a ${props.label}`}
-        isDisabled={props.isDisabled}
-        menuPlacement={props.menuPlacement}
-        menuPosition={props.menuPosition}
-      />
+      {props.creatable ? 
+        <CreatableSelect.default {...selectProps}/> : 
+        <ReactSelect {...selectProps}/>
+      }
     </div>
   )
 }
