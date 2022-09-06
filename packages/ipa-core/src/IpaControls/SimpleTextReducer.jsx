65275a6ede687da53ac0300ccd16f3a291020e29
adam-invicara <adam.reilly@invicara.com>
2022-09-06 14:18:27 +0400

    creatation of text reducer componenet

--- packages/ipa-core/src/IpaControls/SimpleTextReducer.jsx
+++ packages/ipa-core/src/IpaControls/SimpleTextReducer.jsx
@@ -3,18 +3,24 @@ import { Tooltip } from "@material-ui/core"
 
 const SimpleTextReducer = ({text, limit}) => { 
     const [newText, setNewText] = useState(text)
+    const [underLimit, setUnderLimit] = useState(false)
 
     useEffect(() => {
         if (text.length > limit) {
              setNewText(text.substring(0, limit) + "...")
           } else {
+            setUnderLimit(true)
             return newText
           }
     }, [])
 
-    return <Tooltip title={text}>
-        <i style={{cursor: 'pointer'}}>{newText}</i>
-    </Tooltip>
-}
+    return (
+        <div>
+            {underLimit ? <i>{newText}</i> : 
+            <Tooltip title={text}>
+                <i style={{cursor: 'pointer'}}>{newText}</i>
+            </Tooltip>}
+        </div>
+)}
 
 export default SimpleTextReducer
