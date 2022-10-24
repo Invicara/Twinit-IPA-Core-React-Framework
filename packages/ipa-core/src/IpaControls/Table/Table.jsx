bfb67c289987d45789f0c136e47827adafe95b28
adam-invicara <adam.reilly@invicara.com>
2022-10-24 15:04:39 +0400

    resolved the requested changes

--- packages/ipa-core/src/IpaControls/Table/Table.jsx
+++ packages/ipa-core/src/IpaControls/Table/Table.jsx
@@ -1,25 +1,9 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { propsEqual } from '../../IpaUtils/compare'
 import Cell from './Cell'
 import './Table.scss'
-import {RadioButtonUnchecked as UncheckedCircle, RadioButtonChecked as CheckedRadioCircle} from "@material-ui/icons";
 
-export default function Table ({rows, className, headers, checkbox, options, collectionSelect}) {
-  const [selected, setSelected] = useState(0)
-
-  const selectedCell = (idx) => {
-    collectionSelect(idx)
-    setSelected(idx)
-  }
-
-  function checkboxCell(idx) {
-    return (
-      <td>
-          {idx === selected ? <CheckedRadioCircle style={{color: '#C71784', cursor: 'pointer'}}/> :
-          <UncheckedCircle style={{cursor: 'pointer'}} onClick={() => selectedCell(idx)} />}
-      </td>
-    )
-  }
+export default function Table ({rows, className, headers, options}) {
     
   const hasData = rows && rows.length > 0
   return (
@@ -27,11 +11,6 @@ export default function Table ({rows, className, headers, checkbox, options, col
       <tbody>
         {headers && (
           <tr>
-            {checkbox && 
-              <th>
-                <Cell />
-              </th>
-            }
             {headers?.map(header => {
               return (
                 <th key={header}>
@@ -45,9 +24,6 @@ export default function Table ({rows, className, headers, checkbox, options, col
         {hasData &&
           rows.map((row, idx) => (
             <tr key={idx}> 
-            {checkbox && 
-              checkboxCell(idx)
-            }
               {row.map((cell, idx) => (
                 <td key={idx}>
                   <Cell type={cell.type} val={cell.val} className={cell.className} />
