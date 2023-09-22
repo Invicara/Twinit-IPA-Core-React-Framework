5a2c7c9fc2764b1b07d3f8f1d61ba3c923f9ee10
scottmollon-invicara <scott.mollon@invicara.com>
2023-09-22 11:18:14 -0400

    fix install code block component
    (cherry picked from commit 2df993dbfc3a0c7d1949b6711f6cb78b78827352)

--- packages/ipa-core/docs/docComponents/InstallCodeBlock.jsx
+++ packages/ipa-core/docs/docComponents/InstallCodeBlock.jsx
@@ -14,16 +14,16 @@ export const InstallCodeBlock = (() => {
 
    useEffect(() => {
 
-     let module = moduleMap[siteConfig.customFields.platformName]
+     let module = moduleMap[siteConfig.customFields.platformName.toUpperCase()]
      if (!module) module = moduleMap['TWINIT']
 
      setModuleName(module)
 
    }, [])
 
-   return <BrowserOnly>{() => <div class="codeBlockContainer_J+bg">
+   return <BrowserOnly>{() => <div className="codeBlockContainer_J+bg">
           <div className="codeBlockContent_csEI bash">
-               <pre tabindex="0" className="prism-code language-bash codeBlock_rtdJ thin-scrollbar" style={{color: 'rgb(57, 58, 52)', backgroundColor: 'rgb(246, 248, 250)'}}>
+               <pre className="prism-code language-bash codeBlock_rtdJ thin-scrollbar" style={{color: 'rgb(57, 58, 52)', backgroundColor: 'rgb(246, 248, 250)'}}>
                     <code className="codeBlockLines_1zSZ">
                          <span className="token-line" style={{color: 'rgb(57, 58, 52)'}}>
                               <span className="token function" style={{color: 'rgb(215, 58, 73)'}}>npm</span>
