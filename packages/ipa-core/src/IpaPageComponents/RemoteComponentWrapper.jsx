b36d107bb17fe70633492c6b580f067ab972e7f3
Cesar Fraisseix <cesar.fraisseix@invicara.com>
2023-11-23 15:39:19 +0100

    Fixed remote component in dashboard

--- packages/ipa-core/src/IpaPageComponents/RemoteComponentWrapper.jsx
+++ packages/ipa-core/src/IpaPageComponents/RemoteComponentWrapper.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { RemoteComponent } from "./RemoteComponent";
 
-const RemoteComponentWrapper = props => <RemoteComponent {...props} url={props.config.url} />;
+const RemoteComponentWrapper = props => {
+    console.log("RemoteComponentWrapper props", props)
+    return <RemoteComponent {...props} url={props.url} />
+};
 
 export default RemoteComponentWrapper;
