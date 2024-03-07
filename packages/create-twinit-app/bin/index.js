04b8f2c16a351b9bc125bf15c5dbd06ff7bce33d
scottmollon-invicara <scott.mollon@invicara.com>
2024-03-07 08:25:21 -0500

    [DTF-19] setting all defaults for training app and adding more status and other logging

--- packages/create-twinit-app/bin/index.js
+++ packages/create-twinit-app/bin/index.js
@@ -19,17 +19,17 @@ read(nameOptions).then((appName) => {
          let authorOptions = {prompt: "Author:", default: "None"}
          read(authorOptions).then((author) => {
 
-            let appidOptions = {prompt: "Application ID (required):", default: "None"}
+            let appidOptions = {prompt: "Application ID (required - default = dev-training):", default: "77b2cdca-c1a3-4d27-9d19-7b5358e3b337"}
             read(appidOptions).then((appId) => {
 
-               let cfgUserTypeOptions = {prompt: "Config _userType: (required)", default: "None"}
+               let cfgUserTypeOptions = {prompt: "Config _userType: (required - default = dev-training)", default: "dev-train"}
                read(cfgUserTypeOptions).then((cfgtype) => {
 
                   let twinitOptions = {prompt: "Twinit URL:", default: "https://sandbox-api.invicara.com"}
                   read(twinitOptions).then((url) => {
 
+                     console.log('--> Extracting source files')
                      decompress(path.join(__dirname, 'starter-app-source.zip'), './', {strip: 1}).then(() => {
-                        console.log(' extraction complete')
          
                         package.name = appName.replaceAll(' ', '-').toLowerCase()
                         package.description = desc
@@ -37,6 +37,8 @@ read(nameOptions).then((appName) => {
                         package.author = author
          
                         fs.writeFileSync('./package.json', JSON.stringify(package, null, 3))
+                        console.log('--> Created package.json file')
+
                         fs.writeFileSync('./app/public/config.js', `const endPointConfig = {
                            itemServiceOrigin: '${url}',
                            passportServiceOrigin: '${url}',
@@ -46,6 +48,9 @@ read(nameOptions).then((appName) => {
                            baseRoot: 'http://localhost:8084',
                            applicationId: '${appId}'
                         }`)
+                        console.log('--> Created ./app/public/config.js file')
+
+
                         fs.writeFileSync('./app/ipaCore/ipaConfig.js', `const ipaConfig = {
                            appName: "${appName}",
                            configUserType: "${cfgtype}",
@@ -61,9 +66,14 @@ read(nameOptions).then((appName) => {
                            }
                         }
                         
-                        export default ipaConfig`)
-                     })
-                     
+                           export default ipaConfig`)
+                        })
+                        console.log('--> Created ./app/ipaCore/ipaConfig.js file')
+
+                        console.log('--> New Twinit React Client App Setup Complete')
+                        console.log('--> Run "npm install" to install node modules')
+                        console.log('--> Run "npm run watch" to start local client')
+                        console.log('--> Client will be served at http://localhost:8084')
                   })
 
                })
