b4a03b1f2a45ae2996779375f0136e4d1dc52198
Alexander Hughes <alexander.hughes@invicara.com>
2024-04-09 11:17:32 +0100

    added functionality to 2 component stories

--- packages/ipa-core/src/stories/IpaControls/FilterControl.stories.jsx
+++ packages/ipa-core/src/stories/IpaControls/FilterControl.stories.jsx
@@ -1,34 +1,96 @@
-import FilterControl from "../../IpaControls/FilterControl";
+import FilterControl, { applyFilters } from "../../IpaControls/FilterControl";
+import React from "react";
+import { action } from "@storybook/addon-actions";
 
 export default {
   title: "Controls/FilterControl",
   component: FilterControl,
 };
 
-const Template = (args) => {
-  return <FilterControl {...args} />;
+// Mock data for the Storybook story
+const mockData = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 30,
+    department: "Engineering",
+    startDate: 1620144000000,
+  }, // May 5, 2021
+  {
+    id: 2,
+    name: "Jane Smith",
+    age: 25,
+    department: "Marketing",
+    startDate: 1630444800000,
+  }, // September 1, 2021
+  {
+    id: 3,
+    name: "Bob Johnson",
+    age: 35,
+    department: "Finance",
+    startDate: 1609459200000,
+  }, // January 1, 2021
+  {
+    id: 4,
+    name: "Alice Williams",
+    age: 28,
+    department: "Engineering",
+    startDate: 1640995200000,
+  }, // January 1, 2022
+];
+
+// Function to apply filters and get filtered data
+const applyFiltersAndGetFilteredData = (filters) => {
+  return applyFilters(mockData, filters, (item, property) => {
+    switch (property) {
+      case "name":
+        return item.name.toLowerCase();
+      case "age":
+        return item.age;
+      case "department":
+        return item.department.toLowerCase();
+      case "startDate":
+        return { val: item.startDate, type: "date", epoch: item.startDate };
+      default:
+        return null;
+    }
+  });
 };
 
-export const Default = Template.bind({});
+// Template function to render FilterControl component with dynamic data
+const Template = (args) => {
+  const [filters, setFilters] = React.useState({});
 
+  const handleFilterChange = (newFilters) => {
+    setFilters(newFilters);
+    const filteredData = applyFiltersAndGetFilteredData(newFilters);
+    action("Filtered Data")(filteredData); // Log filtered data to actions panel
+  };
+
+  return (
+    <FilterControl {...args} filters={filters} onChange={handleFilterChange} />
+  );
+};
+
+// Default story exported as "Default"
+export const Default = Template.bind({});
 Default.args = {
-  className: "assets-filter",
+  availableFilters: {
+    name: { type: "text" },
+    age: { type: "number" },
+    department: { type: "text" },
+    startDate: { type: "date" },
+  },
+  availableOperators: [
+    "equals",
+    "does not equal",
+    "contains",
+    "does not contain",
+    "less than",
+    "greater than",
+    "between",
+    "in",
+    "is not in",
+  ],
   placeholder: "Choose filters",
-  filters: [
-    {
-      name: { op: "contains", value: "Entity" },
-      type: { op: "equals", value: "Type A" },
-      date: { op: "greater than", value: new Date(2023, 0, 1) },
-    },
-  ],
-  availableFilters: [
-    {
-      name: { type: "text" },
-      type: { type: "single-pick", values: ["Type A", "Type B"] },
-      date: { type: "date" },
-    },
-  ],
-  availableOperators: ["equals", "contains", "greater than"],
-  onChange: (filters) => console.log("Filters changed:", filters),
-  entitySingular: "Entity",
 };
--- packages/ipa-core/src/stories/IpaPageComponents/EntityListView.stories.jsx
+++ packages/ipa-core/src/stories/IpaPageComponents/EntityListView.stories.jsx
@@ -1,64 +1,99 @@
-import React from 'react';
-import { Provider } from 'react-redux'
-import {EntityListView} from "../../IpaPageComponents/entities/EntityListView";
+import React, { useState } from "react";
+import { Provider } from "react-redux";
+import { EntityListView } from "../../IpaPageComponents/entities/EntityListView";
 import "./EntityListView.stories.scss";
 import entitiesArray from "./sample_entities_1.json";
 import actionsObj from "./sample_actions_1.json";
 import store from "../../redux/store";
+import { useArgs } from "@storybook/client-api";
 
 export default {
-  title: 'Components/EntityListView',
+  title: "Components/EntityListView",
   component: EntityListView,
   parameters: {
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
-  decorators : [
-    (Story) => (<Provider store={store}><Story/></Provider>)
-  ]
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
 };
 
 const Template = (args) => {
-  return <EntityListView {...args}/>;
+  const [selected, setSelected] = useState([]);
+  const handleChange = (items) => {
+    const filteredData = items
+      .filter((item) => item.checked === true) // Filter to include only checked items
+      .map(({ _id }) => ({ _id }));
+
+    setSelected(filteredData);
+
+    //updateArgs({ ...args, selectedEntities: filteredData });
+  };
+  return (
+    <EntityListView
+      {...args}
+      onChange={handleChange}
+      selectedEntities={selected}
+    />
+  );
 };
+
+const mockEntities = [
+  {
+    _id: "1",
+    name: "Entity A",
+    type: "Type 1",
+    url: "https://example.com/a",
+  },
+  {
+    _id: "2",
+    name: "Entity B",
+    type: "Type 2",
+    url: "https://example.com/b",
+  },
+  {
+    _id: "3",
+    name: "Entity C",
+    type: "Type 1",
+    url: "https://example.com/c",
+  },
+];
 
 export const Default = Template.bind({});
 Default.args = {
   entityPlural: "Animals",
   entitySingular: "Animal",
   context: {
-    "ifefPlatform": {
-      "isIOS": true,
-      "isAndroid": false,
-      "isCordova": false,
-      "transitionTimeOut": 450,
-      "name": "iOS"
+    ifefPlatform: {
+      isIOS: true,
+      isAndroid: false,
+      isCordova: false,
+      transitionTimeOut: 450,
+      name: "iOS",
     },
-    "ifefSnapper": {},
-    "ifefNavDirection": "forward"
+    ifefSnapper: {},
+    ifefNavDirection: "forward",
   },
-  "config": {
-    "name": "EntityListView",
-    "className": "entity-list-view-default",
-    "multiselect": true,
-    "columns": [
-      {
-        "name": "Name",
-        "accessor": "Entity Name"
-      },
-      {
-        "name": "Mark",
-        "accessor": "properties.Mark"
-      },
-      {
-        "name": "BA Name",
-        "accessor": "properties.BA Name"
-      }
-    ]
+  config: {
+    name: "EntityListView",
+    className: "entity-list-view-default",
+    multiselect: true,
+    columns: [
+      { name: "ID", accessor: "_id" },
+      { name: "Name", accessor: "name" },
+      { name: "Description", accessor: "description" },
+      { name: "Date Created", accessor: "createdAt" },
+    ],
   },
-  entities: entitiesArray,
+  entities: mockEntities,
   actions: actionsObj,
-  onDetail: (...args) => console.log("onDetail clicked with args: ",args),
-  onChange: (...args) => console.log("onChange clicked with args: ",args),
-  onSortChange: (...args) => console.log("onSortChange clicked with args: ",args),
-  selectedEntities: []
+  onDetail: (...args) => console.log("onDetail clicked with args: ", args),
+
+  onSortChange: (...args) =>
+    console.log("onSortChange clicked with args: ", args),
+  selectedEntities: [],
 };
