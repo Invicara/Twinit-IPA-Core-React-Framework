29ce231a0f5895500ef408ed562895454ee16e63
Alexander Hughes <alexander.hughes@invicara.com>
2024-04-10 10:54:49 +0100

    remove enhanced fetch control

--- packages/ipa-core/src/stories/IpaControls/CollapsibleTextInput.stories.jsx
+++ packages/ipa-core/src/stories/IpaControls/CollapsibleTextInput.stories.jsx
@@ -1,45 +1,31 @@
 import CollapsibleTextInput from "../../IpaControls/CollapsibleTextInput";
+import { useArgs } from "@storybook/client-api";
 
 export default {
   title: "Controls/CollapsibleTextInput",
   component: CollapsibleTextInput,
-  parameters: {
-    backgrounds: { default: "light" },
-  },
   argTypes: {
-    onChange: {
-      action: "changed",
-    },
+    labelProps: { control: "object" },
+    inputProps: { control: "object" },
   },
 };
 
-const Template = (args) => {
-  return (
-    <div style={{ maxWidth: "200px" }}>
-      <CollapsibleTextInput {...args} />
-    </div>
-  );
-};
+const Template = (args) => <CollapsibleTextInput {...args} />;
 
 export const Default = Template.bind({});
-
 Default.args = {
-  inputProps: {
-    value: "This is the collapsed text", // Initial value when collapsed
-    collapsedText: "This is the...", // Text to show when collapsed
-    onChange: (e, isFocused) => {
-      const [_, updateArgs] = useArgs();
-      updateArgs({
-        ...args,
-        inputProps: {
-          ...args.inputProps,
-          value: e.target.value,
-        },
-      });
-    },
-  },
   labelProps: {
-    text: "Input Label", // Label text
-    className: "custom-label-class", // Custom class for label
+    text: "Description",
+    className: "custom-label",
+    collapsedText: "Click to expand",
+  },
+  inputProps: {
+    value:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    collapsedText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed...",
+    onFocusChange: (isFocused) => {
+      console.log("Focused:", isFocused);
+    },
   },
 };
--- packages/ipa-core/src/stories/IpaControls/EnhancedPickListSelect.stories.jsx
+++ packages/ipa-core/src/stories/IpaControls/EnhancedPickListSelect.stories.jsx
@@ -7,32 +7,16 @@ import { EnhancedPickListSelect } from "../../IpaControls/EnhancedPickListSelect
 storiesOf("Controls/EnhancedPickListSelect", module).add(
   "Dynamic Option Add",
   () => {
-    // Define the initial state for the component
     const [currentValue, setCurrentValue] = useState({});
     const [options, setOptions] = useState([]);
 
-    // Function to handle adding a new option
-    const handleAddOption = () => {
-      const newOption = {
-        value: "new_option_value",
-        label: "New Option",
-      };
-
-      // Update the options list
-      setOptions([...options, newOption]);
-
-      // Update the current value with the new option
-      setCurrentValue({
-        ...currentValue,
-        // You can modify the key 'select_id' based on your actual use case
-        select_id: [newOption], // This adds the new option to the specified select
-      });
-    };
-
     const mockInitialValue = {
-      "Input 1": [{ value: "value1", display: "Value 1" }],
-      "Input 2": [{ value: "value2", display: "Value 2" }],
-      // Add initial values for other selectors as needed
+      "Input 1": [
+        { value: "value1", display: "Add new value to options list" },
+      ],
+      "Input 2": [
+        { value: "value2", display: "Add new value to options list" },
+      ],
     };
 
     return (
