ddb5c3236e8599d123aadc0ef32d036d684500e9
Adam O'Reilly <110822205+adam-invicara@users.noreply.github.com>
2024-08-13 13:00:45 +0400

    Dbm 2263: Resolved entity data panel bugs (#19)
    * resolved data group loading bug
    
    * fix: resolved no data error in 3rd panel

--- packages/ipa-core/src/IpaPageComponents/entities/EntityView.jsx
+++ packages/ipa-core/src/IpaPageComponents/entities/EntityView.jsx
@@ -32,10 +32,14 @@ const tableComponents = {
 
 class EntityView extends React.Component {
 
-    state = {displayDetail: false}
+    state = {
+        displayDetail: false,
+        selectedEntity: []
+    }
 
     openDetail = entity => {
-        this.setState({displayDetail: true})
+        this.setState({displayDetail: true, selectedEntity: entity})
+        this.props.setAvailableDataGroups(entity)
         // this.props.entitiesSelected([entity]);
     }
 
@@ -111,7 +115,7 @@ class EntityView extends React.Component {
                 pageContent = <EntityDetailPanel
                     context={this.context}
                     onSummary={this.openSummary}
-                    entity={this.props.selectedEntities[0]}
+                    entity={this.props.selectedEntities[0] || this.state.selectedEntity}
                     config={this.props.handler.config}
                     actions={actions}
                     availableDataGroups={this.props.availableDataGroups}
