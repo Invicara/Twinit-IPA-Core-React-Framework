8fdde80f9d58b1996a4966963c0f61c7959f15e5
Adam O'Reilly <adam.reilly.ext@invicara.com>
2026-01-05 15:32:38 +0400

    fix: export button is disabled on default

--- packages/ipa-core/src/IpaControls/ActionButton.jsx
+++ packages/ipa-core/src/IpaControls/ActionButton.jsx
@@ -5,9 +5,16 @@ import { Tooltip } from "@mui/material";
 
 
 export default function ActionButton(props) {
-    return <span className={`action-button`}>
+    const { disabled, onClick } = props;
+    
+    const handleClick = (e) => {
+        if (disabled) return;
+        onClick?.(e);
+    };
+
+    return <span className={`action-button${disabled ? ' action-button--disabled' : ''}`}>
         <Tooltip title={props.title}>
-            <i className={props.icon} onClick={props.onClick}/>
+            <i className={props.icon} onClick={handleClick}/>
         </Tooltip>
     </span>
 }
